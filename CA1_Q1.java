6fd13c05f16dc6943bc4d635be2e56937eb35fcc
varunpandey29 <98257202+varunpandey29@users.noreply.github.com>
2023-06-09 22:40:37 +0530

    Add files via upload

---
+++ CA1_Q1.java
@@ -0,0 +1,7 @@
+//Q.1 Write a Java program to print VIT; on Console and then print your Branch on a next line.
+class CA1_Q1{    
+    public static void main(String args[]){    
+        System.out.println("VIT");
+        System.out.println("CSE-core"); 
+   }    
+   }   
