bd3967228fa9be4488eaf85c69f2ca917252ea02
varunpandey29 <98257202+varunpandey29@users.noreply.github.com>
2023-06-09 22:42:56 +0530

    Add files via upload

---
+++ CA2_Q1.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+public class CA2_Q1 {
+    public static void main(String args[]){
+        Frame f = new Frame();
+        Label l = new Label("Hello VIT");
+        f.setSize(200, 200);
+        f.add(l);
+        f.setVisible(true);
+    }
+}
